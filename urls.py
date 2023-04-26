b42937a1d652928d60ba82540dff28476ca01728
veenaaravind <36223532+veenaaravind@users.noreply.github.com>
2023-04-27 01:45:54 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,35 @@
+"""jeeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from jeeb import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+
+urlpatterns = [
+ 
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", views.index.as_view(), name="home"),
+    url(r"^get_data_fromdb", views.getdatafromdb, name="get_data_fromdb"),
+    
+
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
